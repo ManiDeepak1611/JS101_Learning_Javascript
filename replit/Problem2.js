4b3ee59230d5e3c39569c4793cc5d0e047f48ef9
C H A N D U <manideepak1611@gmail.com>
2023-02-06 20:50:27 +0530

    login done

---
+++ replit/Problem2.js
@@ -0,0 +1,8 @@
+var Myname="Darla Sreeramamurthy Mani Deepak";
+console.log(Myname);
+
+var fathername="Darla Siva Ram Prasad";
+console.log(fathername);
+
+var mothername="Darla Venkata Lakshmi";
+console.log(mothername);
---
+++ replit/Problem3.js
@@ -0,0 +1,7 @@
+var name="Darla Sreeramamurthy Mani Deepak";
+
+var age=27;
+
+console.log(name,age);
+
+console.log(typeof(name),typeof(age));
